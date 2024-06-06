003841e0d604087e41ab54abcdb5c3380a765496
“thaotran“ <phtranthao@gmail.com>
2024-06-06 13:29:55 +1000

    Add red border to not matching password fields

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
         if (password.value !== confirmPassword.value) {
             message.textContent = '*Passwords do not match';
             message.classList.remove("hidden");
+            password.classList.add('red-border')
+            confirmPassword.classList.add('red-border')
         } else {
             message.classList.add('hidden');
             message.textContent = 'msg';
+            password.classList.remove('red-border')
+            confirmPassword.classList.remove('red-border')
         }
     }
 
